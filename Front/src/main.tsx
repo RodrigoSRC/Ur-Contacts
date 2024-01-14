c62dd45adcc1dc015548a2a65904a3041085c818
Rodrigo Simões Ribeiro Costa <clancoto2015@hotmail.com>
2024-01-14 20:24:59 -0300

    fixed: problemas de tipagem do main solucionados

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.js'
+import App from './App'
 import { UserProvider } from './providers/UserContext.js'
 import { ContactsListProvider } from './providers/ContactsListContext.js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
 
